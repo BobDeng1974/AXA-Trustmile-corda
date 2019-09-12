f82c3e2273cb129dfd2c2fb219470fc46f40fd21
Manuel Jenny <manuel.jenny@outlook.com>
2019-09-12 13:55:25 +0200

    Store transaction in seperate variable in CarFlowTests

--- workflows/src/test/java/com/sidis/eas/flows/test/CarFlowTests.java
+++ workflows/src/test/java/com/sidis/eas/flows/test/CarFlowTests.java
@@ -54,28 +54,27 @@ public class CarFlowTests extends SidisBaseFlowTests {
     @Test
     @Ignore
     public void update_car() throws Exception {
-
-        StateVerifier verifier1 = StateVerifier.fromTransaction(
-                this.newCarCreateFlow("12.345.678", this.insurance2.party, "42", 7000, 1200, dataJSONString()),
-                this.redCar.ledgerServices);
+        SignedTransaction carCreateTx = this.newCarCreateFlow("12.345.678", this.insurance2.party,
+                "42", 7000, 1200, dataJSONString());
+        StateVerifier verifier1 = StateVerifier.fromTransaction(carCreateTx, this.redCar.ledgerServices);
         CarState car = verifier1
                 .output().one()
                 .one(CarState.class)
                 .object();
 
-        StateVerifier verifier2 = StateVerifier.fromTransaction(
-                this.newCarEventCreateFlow("42", 15000000, 100L, false, dataJSONString()),
-                this.redCar.ledgerServices);
+        SignedTransaction carEventCreate1Tx = this.newCarEventCreateFlow("42", 15000000, 100L,
+                false, dataJSONString());
+        StateVerifier verifier2 = StateVerifier.fromTransaction(carEventCreate1Tx, this.redCar.ledgerServices);
         CarEventState carEvent1 = verifier2.output().one().one(CarEventState.class).object();
 
-        StateVerifier verifier3 = StateVerifier.fromTransaction(
-                this.newCarEventCreateFlow("42", 15000000, 90L, false, dataJSONString()),
-                this.redCar.ledgerServices);
+        SignedTransaction carEventCreate2Tx = this.newCarEventCreateFlow("42", 15000000, 90L,
+                false, dataJSONString());
+        StateVerifier verifier3 = StateVerifier.fromTransaction(carEventCreate2Tx, this.redCar.ledgerServices);
         CarEventState carEvent2 = verifier3.output().one().one(CarEventState.class).object();
 
-        StateVerifier verifier4 = StateVerifier.fromTransaction(
-                this.newCarUpdateFlow("12.345.678", this.insurance2.party, 7000, 1200, dataJSONString()),
-                this.redCar.ledgerServices);
+        SignedTransaction carUpdateTx = this.newCarUpdateFlow("12.345.678", this.insurance2.party,
+                7000, 1200, dataJSONString());
+        StateVerifier verifier4 = StateVerifier.fromTransaction(carUpdateTx, this.redCar.ledgerServices);
         CarState updatedCar = verifier4
                 .output().one()
                 .one(CarState.class)
